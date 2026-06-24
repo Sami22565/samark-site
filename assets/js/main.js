6b381a85548e6fe2c4f9f683e9421bfad5f20bab
Sami22565 <Samiyafeai80@gmail.com>
2026-06-23 22:44:08 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -215,37 +215,37 @@
 					}).trigger('resize.ie-intro-fix');
 				}
 
-// Hide intro on scroll (> small).
-breakpoints.on('>small', function() {
-    $main.unscrollex();
-    $main.scrollex({
-        mode: 'bottom',
-        top: '75vh',
-        bottom: '-150vh',
-        enter: function() {
-            $intro.addClass('hidden');
-        },
-        leave: function() {
-            $intro.removeClass('hidden');
-        }
-    });
-});
+			// Hide intro on scroll (> small).
+			breakpoints.on('>small', function() {
+    			$main.unscrollex();
+    			$main.scrollex({
+        			mode: 'bottom',
+        			top: '75vh',
+        			bottom: '-150vh',
+        			enter: function() {
+           			 $intro.addClass('hidden');
+        			},
+        			leave: function() {
+      			      $intro.removeClass('hidden');
+  			      }
+ 			   });
+			});
 
-// Hide intro on scroll (<= small).
-breakpoints.on('<=small', function() {
-    $main.unscrollex();
-    $main.scrollex({
-        mode: 'middle',
-        top: '45vh',
-        bottom: '-45vh',
-        enter: function() {
-            $intro.addClass('hidden');
-        },
-        leave: function() {
-            $intro.removeClass('hidden');
-        }
-    });
-});
+			// Hide intro on scroll (<= small).
+			breakpoints.on('<=small', function() {
+			    $main.unscrollex();
+			    $main.scrollex({
+			        mode: 'middle',
+			        top: '45vh',
+			        bottom: '-45vh',
+   			     enter: function() {
+   			         $intro.addClass('hidden');
+   			     },
+  			      leave: function() {
+  			          $intro.removeClass('hidden');
+    			    }
+			    });
+			});
 
 		}
 
